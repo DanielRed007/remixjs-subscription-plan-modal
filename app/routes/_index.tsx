005519f7f802dafc3385e555d02a72451134f474
Daniel Mesu <danielrojo1927@gmail.com>
2024-10-10 17:33:33 +0100

    adding modal styles

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,4 @@
-import { useState } from "react";
 import type { MetaFunction } from "@remix-run/node";
-import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
-import { XMarkIcon } from "@heroicons/react/24/outline"; // Importing the Heroicon
-import { PlanBox } from "~/components/PlanBox";
 import { SubscriptionModal } from "~/components/SubscriptionModal";
 import { useModal } from "../context/ModalContext";
 
@@ -17,18 +13,39 @@ export default function Index() {
   const { isOpen, openModal } = useModal();
 
   const cards = [
-    { discount: 0, plan: "Interlink Free" },
-    { discount: 20, plan: "Interlink Plus" },
-    { discount: 35, plan: "Interlink Unlimited" },
+    {
+      discount: 0,
+      plan: "Interlink Free",
+      discountedPrice: 0,
+      finalPrice: 0,
+      savedAmount: 0,
+      type: "free",
+    },
+    {
+      discount: 20,
+      plan: "Interlink Plus",
+      discountedPrice: 5.01,
+      finalPrice: 9.99,
+      savedAmount: 60.12,
+      type: "plus",
+    },
+    {
+      discount: 35,
+      plan: "Interlink Unlimited",
+      discountedPrice: 7.44,
+      finalPrice: 14.99,
+      savedAmount: 89.28,
+      type: "unlimited",
+    },
   ];
 
   return (
     <div className='w-full h-screen flex justify-center items-center bg-gray-800'>
       <button
         onClick={openModal}
-        className='px-4 py-2 text-white bg-blue-500 hover:bg-blue-600 rounded-md'
+        className='px-4 py-2 text-white bg-purple-500 hover:bg-purple-600 rounded-md'
       >
-        View Subscription Plans
+        View Interlink Plans
       </button>
 
       <SubscriptionModal isOpen={isOpen} cards={cards} />
